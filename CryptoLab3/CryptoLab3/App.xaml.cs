ce8b152aaa26110460e2dd98a3a42b795a547e0d
DESKTOP-NKGR7AM\User <manyak.yuriy@gmail.com>
2017-12-17 05:26:01 +0200

    - Added NLog configs to the UI project - Added .gitignore to the solution

--- CryptoLab3/CryptoLab3/App.xaml.cs
+++ CryptoLab3/CryptoLab3/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using NLog;
 using System.Windows;
 
 namespace CryptoLab3
@@ -13,5 +8,16 @@ namespace CryptoLab3
     /// </summary>
     public partial class App : Application
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public App()
+        {
+            logger.Info("Crypto UI has started.");
+
+            Exit += delegate
+            {
+                logger.Info("Crypto UI has finished.");
+            };
+        }
     }
 }
