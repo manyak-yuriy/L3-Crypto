65ba40f19de03df57be613c8c1d2b5596deb41af
DESKTOP-NKGR7AM\User <manyak.yuriy@gmail.com>
2017-12-17 13:48:25 +0200

    Add contracts project

---
+++ CryptoLab3/RSALab.Contracts/RSAPrivateKey.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSALab.Contracts
+{
+    public class RSAPrivateKey
+    {
+        public RSAPrivateKey(BigInteger n, BigInteger d)
+        {
+            N = n;
+            D = d;
+        }
+
+        public BigInteger N { get; private set; }
+
+        public BigInteger D { get; private set; }
+    }
+}
---
+++ CryptoLab3/RSALab.Contracts/RSAPublicKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Numerics;
+
+namespace RSALab.Contracts
+{
+    public class RSAPublicKey
+    {
+        public RSAPublicKey(BigInteger n, BigInteger e)
+        {
+            N = n;
+            E = e;
+        }
+
+        public BigInteger N { get; private set; }
+
+        public BigInteger E { get; private set; }
+    }
+}
