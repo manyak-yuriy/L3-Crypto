28ecda8ee436ffad35dedf14e6178865706e57fe
DESKTOP-NKGR7AM\User <manyak.yuriy@gmail.com>
2017-12-17 13:18:26 +0200

    - Add NLog NuGet packages to all projects - Add Prime Test implementation - Add random big prime implementation

--- CryptoLab3/Lab3DebugConsole/Program.cs
+++ CryptoLab3/Lab3DebugConsole/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog;
+using RSALab.Implementation;
 
 namespace Lab3DebugConsole
 {
     class Program
     {
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
         static void Main(string[] args)
         {
+            var bigRandomPrime = BigIntegerRandom.GetBigRandomPrime();
+
+
+            _logger.Info("Big random prime number: {0}", bigRandomPrime);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ CryptoLab3/RSALab.Implementation/BigIntegerRandom.cs
@@ -0,0 +1,55 @@
+﻿using System.Numerics;
+using System.Security.Cryptography;
+using NLog;
+
+namespace RSALab.Implementation
+{
+    public static class BigIntegerRandom
+    {
+        private const int DefaultBitness = 256;
+
+        private const int Certainty = 5;
+
+        private static Logger _logger = LogManager.GetCurrentClassLogger();
+
+        private static int GetByteLength(int bitness)
+        {
+            return bitness / 8;
+        }
+
+        public static BigInteger GetBigRandom(int bitLength = DefaultBitness)
+        {
+            RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+            int byteLength = GetByteLength(bitLength);
+
+            byte[] bytes = new byte[byteLength];
+            BigInteger a;
+
+            rng.GetBytes(bytes);
+            a = new BigInteger(bytes);
+
+            return BigInteger.Abs(a);
+        }
+
+        public static BigInteger GetBigRandomPrime(int bitLength = DefaultBitness)
+        {
+            BigInteger randomNumber = GetBigRandom(bitLength);
+
+            if (randomNumber % 2 == 0)
+            {
+                randomNumber += 1;
+            }
+
+            while (true)
+            {
+                if (randomNumber.IsProbablePrime(Certainty))
+                {
+                    return randomNumber;
+                }
+
+                randomNumber += 2;
+            }
+        }
+    }
+}
